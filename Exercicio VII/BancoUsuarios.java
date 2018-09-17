2a8461a9448cb329d9e9b188569363ae88689211
Leandro <leandro_santos16@hotmail.com>
2018-09-17 20:50:18 -0300

    Exercicio VII

---
+++ Exercicio VII/BancoUsuarios.java
@@ -0,0 +1,12 @@
+package exercício07;
+
+public interface BancoUsuarios {
+	int quantidadeDeUsuarios = 0;
+	int usuariosConectados = 0;
+
+	public String getNumeroDeUsuarios();
+
+	public String getUsuariosConectados() ;
+	
+}
+
